24ffa139bd989e6ce75dd700158a12017aef6cc5
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-03-15 00:24:28 +0100

    Main component converted in the router root

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import RootLayout from "./pages/RootLayout";
+import HomePage from "./pages/HomePage";
+import LogInPage from "./pages/LogInPage";
+import SignUpPage from "./pages/SignUpPage";
+import { checkAuthLoader, checkUnauthLoader } from "./utils/loaders";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [{ index: true, element: <HomePage />, loader: checkAuthLoader }],
+  },
+  {
+    path: "/login",
+    element: <LogInPage />,
+    loader: checkUnauthLoader,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+    loader: checkUnauthLoader,
+  }
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
