069d363498defefa93b58acda76f0e76d353b381
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-03-16 00:00:19 +0100

    Added loaders to fetch a user by id

--- src/utils/loaders.js
+++ src/utils/loaders.js
@@ -1,4 +1,5 @@
-import { redirect } from "react-router-dom";
+import axios from "axios";
+import { defer, json, redirect } from "react-router-dom";
 
 import { getAuthToken } from "./storage";
 
@@ -20,4 +21,42 @@ export const checkUnauthLoader = () => {
   }
 
   return null;
+};
+
+const fetchUserById = async (user_id) => {
+  const response = await axios.get(`http://127.0.0.1:8000/users/${user_id}`, {
+    headers: {
+      Authorization: `Bearer ${getAuthToken()}`,
+    }
+  });
+
+  if (response.status === 404) {
+    throw json(
+      { message: "User not found!" },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  if (response.statusText !== "OK") {
+    throw json(
+      { message: "Could not fetch the user!" },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  const data = await response.data;
+
+  return data;
+};
+
+export const getUserByIdLoader = async ({ request, params }) => {
+  const id = params.user_id;
+
+  return defer({
+    user: await fetchUserById(id),
+  });
 };
