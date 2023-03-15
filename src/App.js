929ab2a7ae7ae4f3333200ea836de4cea65ad4c2
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-03-15 23:53:59 +0100

    Added Profile and Error pages to the router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import RootLayout from "./pages/RootLayout";
 import HomePage from "./pages/HomePage";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
-import { checkAuthLoader, checkUnauthLoader } from "./utils/loaders";
+import { checkAuthLoader, checkUnauthLoader, getUserByIdLoader } from "./utils/loaders";
+import ErrorPage from "./pages/ErrorPage";
+import ProfilePage from "./pages/ProfilePage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [{ index: true, element: <HomePage />, loader: checkAuthLoader }],
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage />, loader: checkAuthLoader },
+      {
+        path: "/profile",
+        children: [
+          { index: true, element: <ProfilePage /> },
+          {
+            path: ":user_id",
+            id: 'user-details',
+            element: <ProfilePage />,
+            loader: getUserByIdLoader,
+          },
+        ],
+        loader: checkAuthLoader,
+      },
+    ],
   },
   {
     path: "/login",
@@ -25,11 +40,12 @@ export const router = createBrowserRouter([
     path: "/signup",
     element: <SignUpPage />,
     loader: checkUnauthLoader,
-  }
+  },
+  { path: "*", element: <ErrorPage /> },
 ]);
 
 const App = () => {
   return <RouterProvider router={router} />;
-}
+};
 
 export default App;
