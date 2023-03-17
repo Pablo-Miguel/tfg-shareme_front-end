d08be86a8954efbf7a7160ede2237d242d912aa6
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-03-17 02:39:18 +0100

    Added to the store of Redux the stuff store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 
-import authReducer from './auth-store/auth-slice';
+import authReducer from "./auth-store/auth-slice";
+import stuffReducer from "./stuff-store/stuff-slice";
 
 const store = configureStore({
-    reducer: { auth: authReducer }
+  reducer: { auth: authReducer, stuff: stuffReducer },
 });
 
 export default store;
