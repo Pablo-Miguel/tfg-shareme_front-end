66e1ab6ebd9c26f521ae7b456f5fc8110df7cbe7
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-03-17 02:36:19 +0100

    A hook that uses axios to create any http request

---
+++ src/hooks/useHttp.js
@@ -0,0 +1,37 @@
+import { useState, useCallback } from 'react';
+import axios from 'axios';
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await axios({
+        url: requestConfig.url,
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        data: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (response.status !== 200 && response.status !== 201) {
+        throw new Error('Request failed!');
+      }
+
+      applyData(response.data);
+    } catch (err) {
+      setError(err.message || 'Something went wrong!');
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
