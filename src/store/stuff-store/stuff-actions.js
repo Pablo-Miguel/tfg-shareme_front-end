3ba6331a9e120aa9e8ee38c171c630291d754015
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-03 22:38:27 +0200

    Added an acction to like the stuff

--- src/store/stuff-store/stuff-actions.js
+++ src/store/stuff-store/stuff-actions.js
@@ -36,8 +36,13 @@ export const fetchStuff = (page_number = 0, text_searched = "") => {
         return;
       }
 
+      let temp_stuff = [];
+      fetchedStaff.data.stuff.forEach(element => {
+        temp_stuff.push({...element.single_stuff, isLiked: element.isLiked});
+      });
+
       dispatch(stuffActions.setIsLoading(false));
-      dispatch(stuffActions.setStuff(fetchedStaff.data.stuff));
+      dispatch(stuffActions.setStuff(temp_stuff));
       dispatch(stuffActions.setTotal(fetchedStaff.data.total));
     } catch (error) {
       dispatch(
@@ -49,6 +54,29 @@ export const fetchStuff = (page_number = 0, text_searched = "") => {
 
       dispatch(stuffActions.setStuff([]));
       dispatch(stuffActions.setTotal(0));
+    }
+  };
+};
+
+export const likeStuff = (stuff_id) => {
+  return async (dispatch) => {
+    try {
+      dispatch(stuffActions.setError(null));
+
+      const fetchedStuff = await axios.get(`http://127.0.0.1:8000/stuff/${stuff_id}/like`, {
+        headers: {
+          Authorization: `Bearer ${getAuthToken()}`,
+        },
+      });
+      
+      dispatch(stuffActions.likeStuff(fetchedStuff.data));
+    } catch (error) {
+      dispatch(
+        stuffActions.setError({
+          message: "Error liking stuff",
+          status: 404,
+        })
+      );
     }
   };
 };
