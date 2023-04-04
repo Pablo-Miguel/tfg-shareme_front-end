5da87eb01dfa67fd051dd188f3c0e366d0b7ed9a
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-04 16:59:09 +0200

    Preloads the stuff to the store when the app inits

--- src/pages/RootLayout.js
+++ src/pages/RootLayout.js
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 import MainHeader from "../components/MainHeader/MainHeader";
 import SideMenu from "../components/SideMenu/SideMenu";
 import useUser from "../hooks/useUser";
+import useStuff from "../hooks/useStuff";
 
 const RootLayout = () => {
+  useStuff();
   const user = useUser();
 
   return (
