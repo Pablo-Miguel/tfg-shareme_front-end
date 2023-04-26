995c2f1abc4863c02c4db61cc393bfbf055071c5
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-26 23:43:53 +0200

    Added a timer to wait a response (Bug)

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -28,7 +28,9 @@ const DetailsPage = () => {
 
   useEffect(() => {
     if (!current_stuff && !isLoading) {
-      navigate("/");
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
     }
   }, [current_stuff, isLoading, navigate]);
 
@@ -42,36 +44,35 @@ const DetailsPage = () => {
         <h1>Loading...</h1> 
     : 
         <>
-            <h1>Details Page</h1>
-            <img src={current_stuff.image} alt="stuff_image"/>
-            <h2>{current_stuff.title}</h2>
-            <p>{current_stuff.description}</p>
-            <p>Views: {current_stuff.views}</p>
-            <p>Likes: {current_stuff.likes}</p>
-            <p>Category: {current_stuff.category}</p>
-            <p>Owner: {current_stuff.owner}</p>
-            <p>Price: {current_stuff.price}€</p>
-            {current_stuff.has_offer && (
-                <p>
-                    Offer: <span>{current_stuff.offer_price}€</span>
-                </p>
-            )}
-            <a href={current_stuff.shopping_link}  target="_blank" rel="noopener noreferrer">To by click here</a>
-            <p>Creation day: {current_stuff.createdAt}</p>
-            <p>Last update: {current_stuff.updatedAt}</p>
-            <button onClick={likeHandler} disabled={isLikedBool}>Like</button>
-            {/* A divider for commets area */}
-            <hr/>
-            <h2>Comments</h2>
-            <form>
-                <label htmlFor="comment">Write your comment here:</label>
-                <br/>
-                <textarea id="comment" name="comment" cols={80} rows={10}/>
-                <br/>
-                <button type="button">Submit</button>
-            </form>
-            <p>Comments will be here</p>
-
+          <h1>Details Page</h1>
+          <img src={current_stuff.image} alt="stuff_image"/>
+          <h2>{current_stuff.title}</h2>
+          <p>{current_stuff.description}</p>
+          <p>Views: {current_stuff.views}</p>
+          <p>Likes: {current_stuff.likes}</p>
+          <p>Category: {current_stuff.category}</p>
+          <p>Owner: {current_stuff.owner}</p>
+          <p>Price: {current_stuff.price}€</p>
+          {current_stuff.has_offer && (
+              <p>
+                  Offer: <span>{current_stuff.offer_price}€</span>
+              </p>
+          )}
+          <a href={current_stuff.shopping_link}  target="_blank" rel="noopener noreferrer">To by click here</a>
+          <p>Creation day: {current_stuff.createdAt}</p>
+          <p>Last update: {current_stuff.updatedAt}</p>
+          <button onClick={likeHandler} disabled={isLikedBool}>Like</button>
+          {/* A divider for commets area */}
+          <hr/>
+          <h2>Comments</h2>
+          <form>
+              <label htmlFor="comment">Write your comment here:</label>
+              <br/>
+              <textarea id="comment" name="comment" cols={80} rows={10}/>
+              <br/>
+              <button type="button">Submit</button>
+          </form>
+          <p>Comments will be here</p>
         </>
     }
     </>;
