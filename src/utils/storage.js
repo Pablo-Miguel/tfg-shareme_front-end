2e34720bbd9e1707ead9890186c8e00ed1047555
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-26 23:46:32 +0200

    Methods to save the user Id un local storage

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -15,3 +15,21 @@ export const setAuthToken = (token) => {
 export const removeAuthToken = () => {
   localStorage.removeItem("token");
 };
+
+export const getUserId = () => {
+  const user_id = localStorage.getItem("user_id");
+
+  if (!user_id) {
+    return null;
+  }
+
+  return user_id;
+};
+
+export const setUserId = (user_id) => {
+  localStorage.setItem("user_id", user_id);
+}
+
+export const removeUserId = () => {
+  localStorage.removeItem("user_id");
+}
