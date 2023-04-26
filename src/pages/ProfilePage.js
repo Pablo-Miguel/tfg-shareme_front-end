bfd3e59e67f560f3318371f0296258a4f0152f50
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-26 23:44:48 +0200

    This page can now view his stuff

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,40 +1,91 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams, useRouteLoaderData } from "react-router-dom";
 
 import PageContent from "../components/Layouts/PageContent/PageContent";
 import useUser from "../hooks/useUser";
+import StuffCard from "../components/StuffCard/StuffCard";
 
 const ProfilePage = (props) => {
+  const { user_id } = useParams();
   const user = useUser();
-  const laoder = useRouteLoaderData("user-details");
+  const isMe = user_id == user._id;
+  const loader = useRouteLoaderData("user-details");
+  const [ frontUser, setFrontUser ] = useState(null);
+
+  useEffect(() => {
+
+    if(loader){
+
+      if (!isMe) {
+        setFrontUser(loader.user);
+      } else {
+        setFrontUser(user);
+      }
+
+      setFrontUser((prevState) => ({
+        ...prevState,
+        stuff: loader.userStuff,
+        total: loader.userStuff.length
+      }));
+
+    }
+
+  }, [loader]);
 
   return (
     <>
-      {!laoder ? (
-        <PageContent title="Your Profile">
+      {frontUser ? (
+        <div style={
+          {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "100%",
+            height: "100%",
+          }
+        }>
           <div>
+            <h1>{isMe ? "My" : "User"} profile</h1>
             <div>
-              <img src={user.avatar} alt="profile" />
+              <img src={frontUser.avatar} alt="profile" />
             </div>
             <div>
-              <h2>{`${user.firstName} ${user.lastName}`}</h2>
-              <p>Followers: {user.followers}</p>
-              <p>Following: {user.following}</p>
+              <h2>{`${frontUser.firstName} ${frontUser.lastName}`}</h2>
+              <p>Followers: {frontUser.followers}</p>
+              <p>Following: {frontUser.following}</p>
             </div>
           </div>
-        </PageContent>
+          <div>
+            <h3>Posted stuff  </h3>
+            <div>
+              {frontUser.total === 0 && <h1>No stuff found yet!</h1>}
+              {frontUser.stuff.map((item) => (
+                <StuffCard
+                  key={item._id}
+                  id={item._id}
+                  title={item.title}
+                  price={item.price}
+                  img={item.image}
+                  views={item.views}
+                  likes={item.likes}
+                  category={item.category}
+                  owner={item.owner}
+                  has_offer={item.has_offer}
+                  offer_price={item.offer_price}
+                  isLiked={true}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       ) : (
-        <PageContent title="User Profile">
-          <div>
-            <div>
-              <img src={laoder.user.avatar} alt="profile" />
-            </div>
-            <div>
-              <h2>{`${laoder.user.firstName} ${laoder.user.lastName}`}</h2>
-              <p>Followers: {laoder.user.followers}</p>
-              <p>Following: {laoder.user.following}</p>
-            </div>
-          </div>
-        </PageContent>
+        <h1 style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}>Loading...</h1>
       )}
     </>
   );
