d2f7ea628f154984658883dd32528f340f415c6a
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-04-26 23:46:09 +0200

    A method to load the stuff before the profile page

--- src/utils/loaders.js
+++ src/utils/loaders.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { defer, json, redirect } from "react-router-dom";
 
-import { getAuthToken } from "./storage";
+import { getAuthToken, getUserId } from "./storage";
 
 export const checkAuthLoader = () => {
   const token = getAuthToken();
@@ -53,10 +53,48 @@ const fetchUserById = async (user_id) => {
   return data;
 };
 
+const fetchUserStuff = async (user_id, isMe) => {
+
+  let url_base = 'http://127.0.0.1:8000/stuff?';
+
+  if (!isMe) {
+    url_base += `other_user_id=${user_id}`;
+  } else {
+    url_base += `isMine=${isMe}`;
+  }
+
+  const response = await axios.get(url_base, {
+    headers: {
+      Authorization: `Bearer ${getAuthToken()}`,
+    }
+  });
+
+  if (response.statusText !== "OK") {
+    throw json(
+      { message: "Could not fetch the user stuff!" },
+      {
+        status: 500,
+      }
+    );
+  }
+
+  let temp_stuff = [];
+  response.data.stuff.forEach(element => {
+    temp_stuff.push({...element.single_stuff, isLiked: element.isLiked});
+  });
+
+  return temp_stuff;
+
+};
+
+
 export const getUserByIdLoader = async ({ request, params }) => {
+  const isMe = params.user_id == getUserId();
   const id = params.user_id;
 
   return defer({
+    isMe: isMe,
     user: await fetchUserById(id),
+    userStuff: await fetchUserStuff(id, isMe),
   });
 };
