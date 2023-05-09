7ee2460ee0c0241dd7fea4581160c32c5eb53ce5
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-09 23:45:23 +0200

    New page to search for some other profiles

---
+++ src/pages/SearchProfilesPage.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const SearchProfilesPage = () => {
+    
+    return (
+        <div>
+            <h1>Search Profiles</h1>
+        </div>
+    );
+}
+
+export default SearchProfilesPage;
