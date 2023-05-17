056b0e26cfd019a124aba7bf75e596e3398fe057
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-17 23:15:11 +0200

    Now have search bar component

---
+++ src/components/UIs/SearchBar/SearchBar.js
@@ -0,0 +1,28 @@
+import { useRef } from "react";
+import { Button, Input } from "@mui/joy";
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchBar = (props) => {
+    const searchBarInputRef = useRef();
+
+    const onClickHandler = () => {
+        props.onSearch(searchBarInputRef.current.children[1].value);
+    };
+
+    const onChangeHandler = (event) => {
+        props.onSearch(event.target.value);
+    };
+
+    return (
+        <Input
+            startDecorator={<SearchIcon />}
+            endDecorator={<Button onClick={onClickHandler}>Search</Button>}
+            placeholder="Search for stuff..."
+            onChange={onChangeHandler}
+            fullWidth={true}
+            ref={searchBarInputRef}
+        />
+    );
+};
+
+export default SearchBar;
