18ae40859e0049df1295e0d4762aba3a1f27624d
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-17 23:20:32 +0200

    Login page uses now a MUI template

--- src/pages/LogInPage.js
+++ src/pages/LogInPage.js
@@ -5,10 +5,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { login } from "../store/auth-store/auth-actions";
 import { authActions } from "../store/auth-store/auth-slice";
 
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Typography from '@mui/joy/Typography';
+import { Link as JoyLink } from "@mui/joy";
+import LogInSignUpWrapper from "../components/LogInSignUpWrapper/LogInSignUpWrapper";
+
 const LogInPage = () => {
   const dispatch = useDispatch();
-  const emailInputRef = useRef();
-  const passwordInputRef = useRef();
   const error = useSelector((state) => state.auth.error);
 
   useEffect(() => {
@@ -17,47 +24,67 @@ const LogInPage = () => {
     };
   }, [dispatch]);
 
-  const loginHandler = (event) => {
-    event.preventDefault();
-
+  const loginHandler = (data) => {
     dispatch(
       login(
-        emailInputRef.current.value,
-        passwordInputRef.current.value
+        data.email,
+        data.password
       )
     );
   };
 
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const data = {
+      email: formElements.email.value,
+      password: formElements.password.value
+    };
+    loginHandler(data);
+  };
+
   return (
     <>
-      <h1>Log In</h1>
-      <form>
-        <label htmlFor="email">Email </label>
-        <input type="email" name="email" id="email" ref={emailInputRef} />
-        <br />
-        <br />
-        <label htmlFor="password">Password </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          ref={passwordInputRef}
-        />
-        <br />
-        <br />
-        {error && (
-          <div>
-            <p>{error.message}</p>
-          </div>
-        )}
-        <button onClick={loginHandler} type="submit">
-          Log In
-        </button>
-
-        <p>
-          Don't have an account? <Link to={"/signup"}>Sign Up</Link>
-        </p>
-      </form>
+      <LogInSignUpWrapper title='Welcome back' subtitle='Let&apos;s get started! Please enter your details.'>
+        <form
+          onSubmit={onSubmitHandler}
+        >
+          <FormControl required>
+            <FormLabel>Email</FormLabel>
+            <Input placeholder="Enter your email" type="email" name="email" />
+          </FormControl>
+          <FormControl required>
+            <FormLabel>Password</FormLabel>
+            <Input placeholder="•••••••" type="password" name="password" />
+          </FormControl>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Link to="/">
+              <JoyLink fontSize="sm" fontWeight="lg">
+                Forgot password?
+              </JoyLink>
+            </Link>
+            <Link to="/signup">
+              <JoyLink fontSize="sm" fontWeight="lg">
+                Don't have an account jet?
+              </JoyLink>
+            </Link>
+          </Box>
+          <Button type="submit" fullWidth>
+            Sign in
+          </Button>
+          {error && (
+            <Typography level="body2" color="error.main">
+              {error.message}
+            </Typography>
+          )}
+        </form>
+      </LogInSignUpWrapper>
     </>
   );
 };
