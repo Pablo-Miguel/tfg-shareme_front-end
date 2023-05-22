6e21231d57790e8e7218aa9c7a6278c00161b219
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-22 23:11:28 +0200

    Box has been deleted

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Divider } from "@mui/joy";
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 
 import { fetchStuff } from "../store/stuff-store/stuff-actions";
 import StuffCard from "../components/StuffCard/StuffCard";
@@ -28,54 +28,51 @@ const HomePage = () => {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={1} padding={2}>
-        <Grid item xs={12}>
-          <h1>
-            Home Page
-          </h1>
-        </Grid>
-        <Grid item xs={12}>
-          <SearchBar onSearch={searchBarHandler} />
-        </Grid>
-        <Grid item xs={12}>
-          <Divider />
-        </Grid>
-        {!stuff.isLoading && (
-          <>
-            {stuff.total === 0 && (
-              <Grid item xs={12} container justifyContent="center">
-                <h1>No stuff found yet!</h1>
-              </Grid>
-            )}
-            {stuff.stuff.map((item) => (
-              <Grid item xs={12} md={6} lg={4} key={item._id} container justifyContent="center">
-                <StuffCard
-                  id={item._id}
-                  stuff={item}
-                />
-              </Grid>
-            ))}
-            {Math.ceil(stuff.total / stuff.limit) > 1 && (
-              <Grid item xs={12} container justifyContent="center">
-                <PaginationUI page={stuff.page + 1} count={Math.ceil(stuff.total / stuff.limit)} onChange={onChangePaginationHandler} />
-              </Grid>
-            )}
-          </>
-        )}
-        {stuff.isLoading && !stuff.error && (
-          <Grid item xs={12}>
-            <Spinner />
-          </Grid>
-        )}
-        {stuff.error && (
-          <Grid item xs={12} container justifyContent="center">
-            <h1>Error: {stuff.error.status} - {stuff.error.message}</h1>
-          </Grid>
-        )}
+    <Grid container spacing={1} padding={2}>
+      <Grid item xs={12}>
+        <h1>
+          Home Page
+        </h1>
       </Grid>
-    </Box>
-
+      <Grid item xs={12}>
+        <SearchBar onSearch={searchBarHandler} />
+      </Grid>
+      <Grid item xs={12}>
+        <Divider />
+      </Grid>
+      {!stuff.isLoading && (
+        <>
+          {stuff.total === 0 && (
+            <Grid item xs={12} container justifyContent="center">
+              <h1>No stuff found yet!</h1>
+            </Grid>
+          )}
+          {stuff.stuff.map((item) => (
+            <Grid item xs={12} md={6} lg={4} key={item._id} container justifyContent="center">
+              <StuffCard
+                id={item._id}
+                stuff={item}
+              />
+            </Grid>
+          ))}
+          {Math.ceil(stuff.total / stuff.limit) > 1 && (
+            <Grid item xs={12} container justifyContent="center">
+              <PaginationUI page={stuff.page + 1} count={Math.ceil(stuff.total / stuff.limit)} onChange={onChangePaginationHandler} />
+            </Grid>
+          )}
+        </>
+      )}
+      {stuff.isLoading && !stuff.error && (
+        <Grid item xs={12}>
+          <Spinner />
+        </Grid>
+      )}
+      {stuff.error && (
+        <Grid item xs={12} container justifyContent="center">
+          <h1>Error: {stuff.error.status} - {stuff.error.message}</h1>
+        </Grid>
+      )}
+    </Grid>
   );
 };
 
