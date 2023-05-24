26819f00b86623cd8f2f5c94806f8ec67c4a29ad
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-24 23:36:29 +0200

    Implemented RatingCard Component

--- src/components/RatingCommentsList/RatingCommentsList.js
+++ src/components/RatingCommentsList/RatingCommentsList.js
@@ -1,29 +1,48 @@
 import { useState } from "react";
 import { Box, Button, FormControl, FormLabel, Textarea, Typography } from "@mui/joy";
-import { Rating } from "@mui/material";
+import { Grid, Rating } from "@mui/material";
+import RatingCard from "../RatingCard/RatingCard";
 
 const RatingCommentsList = (props) => {
     const [value, setValue] = useState(0);
     const [text, setText] = useState('');
 
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const formElements = event.currentTarget.elements;
+
+        const data = {
+          rating: formElements.rating.value,
+          comment: formElements.comment.value
+        };
+
+        props.onClickSubmit({
+          data: data,
+          setValue: setValue,
+          setText: setText
+        });
+    };
+
     return (
         <>
           <form
-            onSubmit={props.onClickSubmit}
+            onSubmit={onSubmitHandler}
           >
             <FormControl required>
             <FormLabel>Rating</FormLabel>
             <Rating
-              name="simple-controlled"
+              name="rating"
               value={value}
               onChange={(event, newValue) => {
                 setValue(newValue);
               }}
             />
             </FormControl>
-            <FormControl required>
+            <FormControl>
               <FormLabel>Your comment</FormLabel>
               <Textarea
+                name="comment"
                 placeholder="Type something here…"
                 value={text}
                 onChange={(event) => setText(event.target.value)}
@@ -51,34 +70,26 @@ const RatingCommentsList = (props) => {
               />
             </FormControl>
           </form>
-          <div className="rating-comments">
-            <div style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              flexDirection: "column"
-            }}>
-              {props.stuff.ratingComments.length !== 0 ? props.stuff.ratingComments.map((ratingComment) => {
-                return (
-                  <div style={
-                    {
-                      marginTop: "1rem",
-                    }
-                  } key={ratingComment._id}>
-                    <div>
-                      <p>Rating: {ratingComment.rating}</p>
-                      <p>Comment: {ratingComment.comment}</p>
-                      <p>From: {ratingComment.from.name}</p>
-                      <p>Created at: {ratingComment.createdAt}</p>
-                      <hr/>
-                    </div>
-                  </div>
-                )})
-              : 
-                <h3>No rating comments yet</h3>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography level="h5" variant="h6" sx={{ mt: 4 }}>Comments ({props.stuff.ratingComments.length})</Typography>
+            </Grid>
+            <Grid item container xs={12}>
+              {
+                props.stuff.ratingComments.length !== 0 ? props.stuff.ratingComments.map((ratingComment) => {
+                  return (
+                    <Grid item xs={12} md={6} container justifyContent="center" key={ratingComment._id} style={{ padding: 10 }}>
+                      <RatingCard rating={ratingComment.rating} comment={ratingComment.comment} from={ratingComment.from} userId={props.userId} />
+                    </Grid>
+                  )}
+                ) : (
+                  <Grid item container justifyContent="center">
+                    <Typography level="h4" variant="h6" sx={{ mt: 4}}>No comments yet</Typography>
+                  </Grid>
+                )
               }
-            </div>
-          </div>
+            </Grid>
+          </Grid>
         </>
     )
 };
