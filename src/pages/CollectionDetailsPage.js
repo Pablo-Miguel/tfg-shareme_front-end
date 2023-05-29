c6f504a6518b47c23fd2e571298515bb9aae4d74
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-29 23:14:21 +0200

    SM-7 CollectionDetailsPage now implements MUI

--- src/pages/CollectionDetailsPage.js
+++ src/pages/CollectionDetailsPage.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useRouteLoaderData } from "react-router-dom";
+import { Divider, Grid, Typography } from "@mui/material";
+
+
 
 import StuffCard from "../components/StuffCard/StuffCard";
 import useHttp from "../hooks/useHttp";
 import { getAuthToken } from "../utils/storage";
+import useUser from "../hooks/useUser";
+import CollectionHeaderBodyDetail from "../components/CollectionHeaderBodyDetail/CollectionHeaderBodyDetail";
+import CollectionBodyDetail from "../components/CollectionBodyDetail/CollectionBodyDetail";
 
 const CollectionDetailsPage = () => {
+    const user = useUser();
     const loader = useRouteLoaderData("collection-details");
     const [ collection, setCollection ] = useState(loader.collection); 
     const effectExecutedRef = useRef(false);
     const { sendRequest: sendCollectionView } = useHttp();
+    const { sendRequest: sendLike } = useHttp();
+    const { sendRequest: sendUnlike } = useHttp();
+    const isMine = collection.owner._id === user._id;
 
     useEffect(() => {
         if (!loader || effectExecutedRef.current) {
@@ -37,49 +47,99 @@ const CollectionDetailsPage = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const likeHandler = () => {
+        if(collection.isLiked) {
+            
+            sendUnlike({
+                url: `${process.env.REACT_APP_BACKEND_BASE_URL}/collections/${collection._id}/unlike`,
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${getAuthToken()}`
+                }
+            }, (data) => {
+                setCollection((prevState) => ({
+                    ...prevState,
+                    isLiked: false,
+                    likes: data.likes
+                }));
+            });
+
+        } else {
+
+            sendLike({
+                url: `${process.env.REACT_APP_BACKEND_BASE_URL}/collections/${collection._id}/like`,
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${getAuthToken()}`
+                }
+            }, (data) => {
+                setCollection((prevState) => ({
+                    ...prevState,
+                    isLiked: true,
+                    likes: data.likes
+                }));
+            });
+            
+        }
+    }
+
     return (
-        <div>
-            <h1>Collection Details Page</h1>
-            {collection ?
-                <div>
-                    <div>
-                        <h2>{collection.title}</h2>
-                        <h3>{collection.description}</h3>
-                        <h4>Views: {collection.views}</h4>
-                        <h4>Owner: {collection.owner.nickName}</h4>
+        <Grid container spacing={3} padding={2}>
+            <Grid item xs={0} md={1} lg={2}></Grid>
+            <Grid item xs={12} md={10} lg={8} container>
+                <Grid item xs={12}>
+                    <Typography variant="h4" component="h4" style={{ fontWeight: "bold"}}>
+                        Collection Details Page
+                    </Typography>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Divider style={{ marginTop: 10, marginBottom: 10 }}/>
+                </Grid>
+                
+                <Grid item xs={12} sm={6} md={4} container justifyContent="center">
+                    <CollectionHeaderBodyDetail collection={collection} />
+                </Grid>
+
+                <Grid item xs={12} sm={6} md={8}>
+                    <CollectionBodyDetail collection={collection} onLike={likeHandler} user={user} />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Divider style={{ marginTop: 10, marginBottom: 10 }}/>
+                </Grid>
+
+                <Grid item xs={12}>
+                    <Typography variant="h5" component="h5"
+                        style={{ fontWeight: "bold" }}
+                    >
+                        Stuff
+                    </Typography>
+
+                    <Grid container spacing={2}>
                         { collection.stuff.length > 0 ?
                             collection.stuff.map((item) => (
-                                <StuffCard
-                                    key={item._id}
-                                    id={item._id}
-                                    stuff={item}
-                                />
+                                <Grid item xs={12} md={6} lg={6} key={item._id} container justifyContent="center">
+                                    <StuffCard
+                                        id={item._id}
+                                        stuff={item}
+                                        isMine={isMine}
+                                    />
+                                </Grid>
                             ))
-                        :
-                            <h1>This collection hasn't have stuff jet</h1>
+                        : 
+                            <Grid item xs={12} container justifyContent="center">
+                                <Typography variant="h6" component="h6" >
+                                    This collection is empty
+                                </Typography>
+                            </Grid>
                         }
-                    </div>
-                    {/* <div>
-                        <h3>Delete stuff</h3>
-                        <form>
-                            {collection.stuff.length > 0 ? 
-                                collection.stuff.map((stuff) => (
-                                    <div key={stuff._id}>
-                                        <input type="checkbox" id={stuff._id} name={stuff._id} value={stuff._id} />
-                                        <label htmlFor={stuff._id}>{stuff.title}</label>
-                                        <br />
-                                    </div>
-                                ))
-                            :
-                                <h1>No stuff found</h1>
-                            }
-                        </form>
-                    </div> */}
-                </div>
-            :
-                <h2>Loading...</h2>
-            }
-        </div>
+                    </Grid>
+                </Grid>
+
+            </Grid>
+            <Grid item xs={0} md={1} lg={2}></Grid>
+        </Grid>
     );
 };
 
