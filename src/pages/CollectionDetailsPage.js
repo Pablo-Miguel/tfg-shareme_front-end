c26a8566dde56ad752c2ac77c626df835a043636
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-29 21:40:03 +0200

    SM-7 Collection details page can now send views to the backend

--- src/pages/CollectionDetailsPage.js
+++ src/pages/CollectionDetailsPage.js
@@ -1,10 +1,41 @@
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useRouteLoaderData } from "react-router-dom";
+
 import StuffCard from "../components/StuffCard/StuffCard";
+import useHttp from "../hooks/useHttp";
+import { getAuthToken } from "../utils/storage";
 
 const CollectionDetailsPage = () => {
     const loader = useRouteLoaderData("collection-details");
-    const [ collection ] = useState(loader.collection); 
+    const [ collection, setCollection ] = useState(loader.collection); 
+    const effectExecutedRef = useRef(false);
+    const { sendRequest: sendCollectionView } = useHttp();
+
+    useEffect(() => {
+        if (!loader || effectExecutedRef.current) {
+            return;
+        }
+
+        effectExecutedRef.current = true;
+
+        sendCollectionView(
+            {
+                url: `${process.env.REACT_APP_BACKEND_BASE_URL}/collections/${collection._id}/view`,
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${getAuthToken()}`
+                }
+            },
+            (data) => {
+                setCollection((prevState) => ({
+                    ...prevState,
+                    views: data.views
+                }));
+            }
+
+        );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <div>
@@ -14,6 +45,7 @@ const CollectionDetailsPage = () => {
                     <div>
                         <h2>{collection.title}</h2>
                         <h3>{collection.description}</h3>
+                        <h4>Views: {collection.views}</h4>
                         <h4>Owner: {collection.owner.nickName}</h4>
                         { collection.stuff.length > 0 ?
                             collection.stuff.map((item) => (
