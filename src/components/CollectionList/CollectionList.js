f5590e84fda4788ea34125247f27c1166632fe1d
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-31 00:50:11 +0200

    SM-6 Created CollectionList component

---
+++ src/components/CollectionList/CollectionList.js
@@ -0,0 +1,112 @@
+import { Grid, Typography } from "@mui/material";
+
+import SearchBar from "../UIs/SearchBar/SearchBar";
+import Spinner from "../Spinner/Spinner";
+import useHttp from "../../hooks/useHttp";
+import { getAuthToken } from "../../utils/storage";
+import PaginationUI from "../UIs/Pagination/PaginationUI";
+import { useState } from "react";
+import CollectionCard from "../CollectionCard/CollectionCard";
+
+const LIMIT = 10;
+
+const CollectionList = ({ collections: initCollections, total: initTotal, isMe, user_id, setFrontUser }) => {
+    const [ collections, setCollections ] = useState(initCollections);
+    const [ total, setTotal ] = useState(initTotal);
+    const [ page, setPage ] = useState(0);
+    const [ textSearched, setTextSearched ] = useState("");
+    const { isLoading, sendRequest: fetchCollections } = useHttp();
+
+    const fetchSearchedCollections = (text_searched, cntPage) => {
+
+        let url_base = `${process.env.REACT_APP_BACKEND_BASE_URL}/collections?`;
+
+        if (!isMe) {
+            url_base += `other_user_id=${user_id}`;
+        } else {
+            url_base += `isMine=${isMe}`;
+        }
+
+        if (text_searched && text_searched !== "") {
+            url_base += `&text_searched=${text_searched}`;
+        }
+
+        url_base += `&limit=10`;
+        url_base += `&skip=${cntPage * LIMIT}`;
+        url_base += '&sortBy=createdAt:desc';
+
+        fetchCollections({
+            url: url_base,
+            method: "GET",
+            headers: {
+            Authorization: `Bearer ${getAuthToken()}`,
+            }
+        }, (data) => {
+
+            if(setFrontUser){
+                setFrontUser((prevState) => ({
+                    ...prevState,
+                    collections: data.collections,
+                    total_collections: data.total
+                }));
+            }
+            
+            setCollections(data.collections);
+            setTotal(data.total);
+
+        });
+
+        setTextSearched(text_searched);
+        setPage(cntPage);
+
+    };
+
+    const onSearchHandler = (text_searched) => {
+        fetchSearchedCollections(text_searched, page);
+    };
+
+    const onChangePaginationHandler = (event, page) => {
+        setPage(page - 1);
+        fetchSearchedCollections(textSearched, page - 1);
+    };
+    
+    return (
+        <Grid container style={{ marginTop: 20 }}>
+            <SearchBar onSearch={onSearchHandler}/>
+            {
+                !isLoading ? (
+                    <>
+                        { (total === 0 || collections.length === 0) && 
+                            <Grid item xs={12} container justifyContent="center" display="flex">
+                                <Typography variant="h5" component="h5" style={{ fontWeight: "bold", marginTop: 100 }}>
+                                    No collections found yet!
+                                </Typography>
+                            </Grid>
+                        }
+                        {collections.map((item) => (
+                            <Grid item container xs={12} md={6} lg={6} key={item._id} justifyContent="center">
+                                <CollectionCard 
+                                    id={item._id} 
+                                    collection={item} 
+                                    isMine={isMe} 
+                                />
+                            </Grid>
+                        ))}
+                    </>
+                ) : (
+                    <Grid item xs={12}>
+                        <Spinner />
+                    </Grid>
+                )
+                
+            }
+            {Math.ceil(total / LIMIT) > 1 && (
+              <Grid item xs={12} container justifyContent="center">
+                <PaginationUI page={page + 1} count={Math.ceil(total / LIMIT)} onChange={onChangePaginationHandler} />
+              </Grid>
+            )}
+        </Grid>
+    )
+};
+
+export default CollectionList;
