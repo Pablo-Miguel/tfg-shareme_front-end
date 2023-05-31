45bd998d5118f2096bd01e63139d3c5cfe3a2e8c
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-05-31 17:27:18 +0200

    SM-6 Created ProfileBody components

---
+++ src/components/ProfileBody/ProfileBody.js
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router-dom";
+import { Button, Divider, Grid, Typography } from "@mui/material";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
+const ProfileBody = ({frontUser, isMe, onClickFollow}) => {
+    const navigate = useNavigate(); 
+
+    return (
+        <>
+            <Typography variant="h5" component="h5"
+                style={{ fontWeight: "bold" }}
+            >
+                {frontUser.nickName}
+            </Typography>
+            <Typography variant="body1" component="p">
+                {frontUser.name}
+            </Typography>
+
+            <Divider style={{ marginTop: 10, marginBottom: 10 }}/>
+
+            {
+                isMe ? (
+                <Grid item xs={12} container>
+                    <Button
+                    variant="contained"
+                    color="primary"
+                    disabled={true}
+                    onClick={() => navigate("/")}
+                    >
+                    Edit profile
+                    </Button>
+                </Grid>
+                ) : (
+                <Grid item xs={12} container>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={onClickFollow}
+                        startIcon={frontUser.isFollowing ? <AccountCircleIcon /> : <AccountCircleOutlinedIcon />}
+                    >
+                        {frontUser.isFollowing ? "Stop following" : "Follow"}
+                    </Button>
+                </Grid>
+                )
+            }
+        </>
+    );
+};
+
+export default ProfileBody;
