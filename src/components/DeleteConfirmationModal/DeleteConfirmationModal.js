54b3cd5271fb334968ab0ab06511c87eecc36524
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-06-01 23:59:26 +0200

    SM-12 Created a Modal to confirm the action of delete something

---
+++ src/components/DeleteConfirmationModal/DeleteConfirmationModal.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Divider from '@mui/joy/Divider';
+import Modal from '@mui/joy/Modal';
+import ModalDialog from '@mui/joy/ModalDialog';
+import WarningRoundedIcon from '@mui/icons-material/WarningRounded';
+import Typography from '@mui/joy/Typography';
+
+const DeleteConfirmationModal = ({ open, setOpen, type, onDelete }) => {
+
+    const onDeleteHandler = () => {
+        onDelete();
+        setOpen(false);
+    };
+
+    return (
+        <Modal open={open} onClose={() => setOpen(false)}>
+            <ModalDialog
+                variant="outlined"
+                role="alertdialog"
+                aria-labelledby="alert-dialog-modal-title"
+                aria-describedby="alert-dialog-modal-description"
+            >
+                <Typography
+                id="alert-dialog-modal-title"
+                component="h2"
+                startDecorator={<WarningRoundedIcon />}
+                >
+                    Confirmation
+                </Typography>
+                <Divider />
+                <Typography id="alert-dialog-modal-description" textColor="text.tertiary">
+                    Are you sure you want to delete your {type}?
+                </Typography>
+                <Box sx={{ display: 'flex', gap: 1, justifyContent: 'flex-end', pt: 2 }}>
+                <Button variant="plain" color="neutral" onClick={() => setOpen(false)}>
+                    Cancel
+                </Button>
+                <Button variant="solid" color="danger" onClick={onDeleteHandler}>
+                    Delete {type}
+                </Button>
+                </Box>
+            </ModalDialog>
+        </Modal>
+    );
+};
+
+export default DeleteConfirmationModal;
