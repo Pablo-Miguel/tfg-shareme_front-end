436485c97224fe3ef728c39de93ce0dd63e2f54a
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-06-05 22:15:00 +0200

    SM-13 Added Edit and Delete functionality at CollectionCard component

--- src/components/CollectionCard/CollectionCard.js
+++ src/components/CollectionCard/CollectionCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Avatar, Card, CardOverflow, Divider, Typography } from "@mui/joy";
 
@@ -10,11 +10,30 @@ import ImgCarousel from "../UIs/ImgCarousel/ImgCarousel";
 import useHttp from "../../hooks/useHttp";
 import { getAuthToken } from "../../utils/storage";
 import { Grid } from "@mui/material";
+import Alert from "../Alert/Alert";
+import DeleteConfirmationModal from "../DeleteConfirmationModal/DeleteConfirmationModal";
 
 const CollectionCard = ({ collection: inputCollection, isMine }) => {
     const [ collection, setCollection ] = useState(inputCollection);
     const navigate = useNavigate();
     const { sendRequest: sendLike } = useHttp();
+    const { error, sendRequest: fetchDeleteCollection } = useHttp();
+    const [ openModal, setOpenModal ] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        if (error && error !== '') {
+            setOpen(true);
+        }
+    }, [error]);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        
+        setOpen(false);
+    };
 
     const profileHandler = () => {
         navigate(`/profile/${collection.owner._id}`, { replace: true });
@@ -42,107 +61,144 @@ const CollectionCard = ({ collection: inputCollection, isMine }) => {
         
     }
 
+    const onEditClickHandler = () => {
+        navigate(`/edit-collection/${collection._id}`, { replace: true });
+    }
+
+    const onDeleteClickHandler = () => {
+        setOpenModal(true);
+    }
+
+    const onDeleteHandler = () => {
+
+        fetchDeleteCollection(
+          {
+            url: `${process.env.REACT_APP_BACKEND_BASE_URL}/collections/${collection._id}`,
+            method: "DELETE",
+            headers: {
+              Authorization: `Bearer ${getAuthToken()}`,
+            },
+          },
+          (data) => {
+            setOpen(true);
+            setTimeout(() => {
+              navigate(0);
+            }, 1200);
+          }
+        );
+    
+      }
+
     return (
-        <Card variant="outlined" className={classes.card}>
-            <CardOverflow 
-                variant="soft"
-                sx={{
+        <>
+            <Card variant="outlined" className={classes.card}>
+                <CardOverflow 
+                    variant="soft"
+                    sx={{
+                        display: "flex",
+                        gap: 1.5,
+                        py: 1.5,
+                        px: "var(--Card-padding)",
+                        bgcolor: "background.level1",
+                    }}
+                >
+                    <CardOverflow sx={{ mt: "auto", mb: "auto" }}
+                        onClick={profileHandler}
+                        style={{
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: 1.5
+                        }}
+                    >
+                        <Avatar src={collection.owner.avatar} alt={collection.owner.nickName} 
+                            style={{ width: 25, height: 25, marginRight: 10 }}
+                        />
+                        <Typography
+                            level="body1"
+                            sx={{
+                            fontWeight: "md",
+                            color: "text.secondary",
+                            }}
+                        >
+                            {collection.owner.nickName}
+                        </Typography>
+                    </CardOverflow>
+                    {
+                        isMine && (
+                            <CardOverflow sx={{ ml: "auto" }}>
+                                <PositionedMenu onEditClick={onEditClickHandler} onDeleteClick={onDeleteClickHandler} />
+                            </CardOverflow>
+                        )
+                    }
+                </CardOverflow>
+
+                <Divider />
+
+                <CardOverflow>
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <ImgCarousel stuff={collection.stuff.slice(0, 5)} />
+                        </Grid>
+                    </Grid>
+                    <RedEyeIconBtn onClick={detailsHandler} />
+                    <FavoriteIconBtn isLiked={collection.isLiked} onClick={likeHandler} isMine={isMine} />
+                </CardOverflow>
+
+                <CardOverflow
+                    sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    gap: 6,
+                    py: 1.5,
+                    px: "var(--Card-padding)",
+                    bgcolor: "background.level",
+                    }}
+                >
+                    <CardOverflow>
+                        <Typography level="body1" sx={{ fontSize: "md", mt: 0.5, mb: 2 }}>
+                            {collection.title}
+                        </Typography>
+                        <Typography level="body2" sx={{ fontSize: "md", mt: 0.5, mb: 2 }}>
+                            Number of stuff: {collection.stuff.length}
+                        </Typography>
+                    </CardOverflow>
+                </CardOverflow>
+
+                <Divider />
+
+                <CardOverflow
+                    variant="soft"
+                    sx={{
                     display: "flex",
                     gap: 1.5,
                     py: 1.5,
                     px: "var(--Card-padding)",
                     bgcolor: "background.level1",
-                }}
-            >
-                <CardOverflow sx={{ mt: "auto", mb: "auto" }}
-                    onClick={profileHandler}
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        gap: 1.5
                     }}
                 >
-                    <Avatar src={collection.owner.avatar} alt={collection.owner.nickName} 
-                        style={{ width: 25, height: 25, marginRight: 10 }}
-                    />
                     <Typography
-                        level="body1"
-                        sx={{
-                        fontWeight: "md",
-                        color: "text.secondary",
-                        }}
+                        level="body3"
+                        sx={{ fontWeight: "md", color: "text.secondary" }}
                     >
-                        {collection.owner.nickName}
+                        {collection.views} views
+                    </Typography>
+                    <Divider orientation="vertical" />
+                    <Typography
+                        level="body3"
+                        sx={{ fontWeight: "md", color: "text.secondary" }}
+                    >
+                        {collection.likes} likes
                     </Typography>
                 </CardOverflow>
-                {
-                    isMine && (
-                        <CardOverflow sx={{ ml: "auto" }}>
-                            <PositionedMenu />
-                        </CardOverflow>
-                    )
-                }
-            </CardOverflow>
-
-            <Divider />
-
-            <CardOverflow>
-                <Grid container>
-                    <Grid item xs={12}>
-                        <ImgCarousel stuff={collection.stuff.slice(0, 5)} />
-                    </Grid>
-                </Grid>
-                <RedEyeIconBtn onClick={detailsHandler} />
-                <FavoriteIconBtn isLiked={collection.isLiked} onClick={likeHandler} isMine={isMine} />
-            </CardOverflow>
-
-            <CardOverflow
-                sx={{
-                display: "flex",
-                flexDirection: "row",
-                gap: 6,
-                py: 1.5,
-                px: "var(--Card-padding)",
-                bgcolor: "background.level",
-                }}
-            >
-                <CardOverflow>
-                    <Typography level="body1" sx={{ fontSize: "md", mt: 0.5, mb: 2 }}>
-                        {collection.title}
-                    </Typography>
-                    <Typography level="body2" sx={{ fontSize: "md", mt: 0.5, mb: 2 }}>
-                        Number of stuff: {collection.stuff.length}
-                    </Typography>
-                </CardOverflow>
-            </CardOverflow>
-
-            <Divider />
-
-            <CardOverflow
-                variant="soft"
-                sx={{
-                display: "flex",
-                gap: 1.5,
-                py: 1.5,
-                px: "var(--Card-padding)",
-                bgcolor: "background.level1",
-                }}
-            >
-                <Typography
-                    level="body3"
-                    sx={{ fontWeight: "md", color: "text.secondary" }}
-                >
-                    {collection.views} views
-                </Typography>
-                <Divider orientation="vertical" />
-                <Typography
-                    level="body3"
-                    sx={{ fontWeight: "md", color: "text.secondary" }}
-                >
-                    {collection.likes} likes
-                </Typography>
-            </CardOverflow>
-        </Card>
+            </Card>
+            <DeleteConfirmationModal open={openModal} setOpen={setOpenModal} type='stuff' onDelete={onDeleteHandler} />
+            {open && !error ? (
+                <Alert severity="success" open={open} handleClose={handleClose} message="Your stuff has been deleted successfully!" />
+            )
+            : (
+                <Alert severity="error" open={open} handleClose={handleClose} message={error} />
+            )}
+        </>
     );
 };
 
