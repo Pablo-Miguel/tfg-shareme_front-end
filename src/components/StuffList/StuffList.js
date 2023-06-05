a41c002c07e9124517904cc9530a78d7875c11ea
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-06-05 22:20:11 +0200

    SM-13 Edited to be implemented with edit collections

--- src/components/StuffList/StuffList.js
+++ src/components/StuffList/StuffList.js
@@ -7,65 +7,112 @@ import useHttp from "../../hooks/useHttp";
 import { getAuthToken } from "../../utils/storage";
 import PaginationUI from "../UIs/Pagination/PaginationUI";
 import { useState } from "react";
+import SelectFormControl from "../UIs/SelectFormControl/SelectFormControl";
 
 const LIMIT = 10;
 
-const StuffList = ({ stuff, total, isMe, user_id, setFrontUser }) => {
+const categories = [
+    "All",
+    "Music",
+    "Photography",
+    "Technology",
+    "Clothes",
+    "Kitchen",
+    "Sports",
+    "Decoration",
+    "Books",
+    "Other"
+];
+
+const StuffList = ({ stuff, total, isMe, user_id, setFrontUser, setData, url_base }) => {
     const [ page, setPage ] = useState(0);
     const [ textSearched, setTextSearched ] = useState("");
+    const [ category, setCategory ] = useState("All");
     const { isLoading, sendRequest: fetchStuff } = useHttp();
 
-    const fetchSearchedStuff = (text_searched, cntPage) => {
+    const fetchSearchedStuff = (text_searched, cntPage, category) => {
+        
+        let urlBase = url_base ? url_base : `${process.env.REACT_APP_BACKEND_BASE_URL}/stuff?`;
 
-        let url_base = `${process.env.REACT_APP_BACKEND_BASE_URL}/stuff?`;
+        urlBase += `isMine=${isMe}`;
 
-        if (!isMe) {
-            url_base += `other_user_id=${user_id}`;
-        } else {
-            url_base += `isMine=${isMe}`;
+        if(category && category !== "All") {
+            urlBase += `&category=${category}`;
         }
 
         if (text_searched && text_searched !== "") {
-            url_base += `&text_searched=${text_searched}`;
+            urlBase += `&text_searched=${text_searched}`;
+        }
+        
+        urlBase += `&limit=10`;
+        urlBase += `&skip=${cntPage * LIMIT}`;
+
+        if (!isMe) {
+            urlBase += `other_user_id=${user_id}`;
         }
 
-        url_base += `&limit=10`;
-        url_base += `&skip=${cntPage * LIMIT}`;
-        url_base += '&sortBy=createdAt:desc';
+        if (text_searched && text_searched !== "") {
+            urlBase += `&text_searched=${text_searched}`;
+        }
+        
+        urlBase += '&sortBy=createdAt:desc';
 
         fetchStuff({
-            url: url_base,
+            url: urlBase,
             method: "GET",
             headers: {
-            Authorization: `Bearer ${getAuthToken()}`,
+                Authorization: `Bearer ${getAuthToken()}`,
             }
         }, (data) => {
 
-            setFrontUser((prevState) => ({
-                ...prevState,
-                stuff: data.stuff,
-                total_stuff: data.total
-            }));
+            if(setFrontUser){
+                setFrontUser((prevState) => ({
+                    ...prevState,
+                    stuff: data.stuff,
+                    total_stuff: data.total
+                }));
+            } else {
+                setData(data);
+            }
 
         });
 
         setTextSearched(text_searched);
         setPage(cntPage);
+        setCategory(category);
 
     };
 
     const onSearchHandler = (text_searched) => {
-        fetchSearchedStuff(text_searched, page);
+        fetchSearchedStuff(text_searched, page, category);
     };
 
     const onChangePaginationHandler = (event, page) => {
         setPage(page - 1);
-        fetchSearchedStuff(textSearched, page - 1);
+        fetchSearchedStuff(textSearched, page - 1, category);
     };
+
+    const categoriesOnChangeHandler = (event) => {
+        fetchSearchedStuff(textSearched, page, event.target.innerText);
+    };
+
 
     return (
         <Grid container style={{ marginTop: 20 }}>
-            <SearchBar onSearch={onSearchHandler}/>
+            <Grid item xs={12} container>
+                <Grid item xs={12} md={8} lg={9} style={{ padding: 5 }}>
+                    <SearchBar onSearch={onSearchHandler}/>
+                </Grid>
+                <Grid item xs={12} md={4} lg={3} style={{ padding: 5 }}>
+                    <SelectFormControl
+                        options={categories}
+                        placeholder="Select category…"
+                        name="category"
+                        initialValue={"All"}
+                        onChange={categoriesOnChangeHandler}
+                    />
+                </Grid>
+            </Grid>
             {
                 !isLoading ? (
                     <>
