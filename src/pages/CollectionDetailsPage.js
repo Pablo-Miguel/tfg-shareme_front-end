aea11eb82ffe57f5ccbb24c5e2fc3c446ff1b1e2
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-06-05 22:22:25 +0200

    SM-32 CollectionDetailsPage now can search and has pagination

--- src/pages/CollectionDetailsPage.js
+++ src/pages/CollectionDetailsPage.js
@@ -1,24 +1,41 @@
 import React, { useEffect, useRef, useState } from "react";
-import { useRouteLoaderData } from "react-router-dom";
+import { useNavigate, useRouteLoaderData } from "react-router-dom";
 import { Divider, Grid, Typography } from "@mui/material";
 
-
-
-import StuffCard from "../components/StuffCard/StuffCard";
 import useHttp from "../hooks/useHttp";
 import { getAuthToken } from "../utils/storage";
 import useUser from "../hooks/useUser";
 import CollectionHeaderBodyDetail from "../components/CollectionHeaderBodyDetail/CollectionHeaderBodyDetail";
 import CollectionBodyDetail from "../components/CollectionBodyDetail/CollectionBodyDetail";
+import StuffList from "../components/StuffList/StuffList";
+import Alert from "../components/Alert/Alert";
 
 const CollectionDetailsPage = () => {
     const user = useUser();
     const loader = useRouteLoaderData("collection-details");
+    const navigate = useNavigate();
     const [ collection, setCollection ] = useState(loader.collection); 
     const effectExecutedRef = useRef(false);
     const { sendRequest: sendCollectionView } = useHttp();
     const { sendRequest: sendLike } = useHttp();
+    const { error, sendRequest: fetchDeleteCollection } = useHttp();
     const isMine = collection.owner._id === user._id;
+
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        if (error && error !== '') {
+            setOpen(true);
+        }
+    }, [error]);
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        
+        setOpen(false);
+    };
 
     useEffect(() => {
         if (!loader || effectExecutedRef.current) {
@@ -62,65 +79,83 @@ const CollectionDetailsPage = () => {
             }));
         });
         
-    }
+    };
+
+    const onClickPagination = (data) => {
+        setCollection((prevState) => ({
+            ...prevState,
+            stuff: data.stuff,
+            totalStuff: data.totalStuff
+        }));
+    };
+
+    const onDeleteHandler = (event) => {
+        fetchDeleteCollection(
+            {
+                url: `${process.env.REACT_APP_BACKEND_BASE_URL}/collections/${collection._id}`,
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${getAuthToken()}`,
+                },
+            },
+            (data) => {
+                setOpen(true);
+                setTimeout(() => {
+                    navigate("/");
+                }, 1200);
+            }
+        );
+    };
 
     return (
-        <Grid container spacing={3} padding={2}>
-            <Grid item xs={0} md={1} lg={2}></Grid>
-            <Grid item xs={12} md={10} lg={8} container>
-                <Grid item xs={12}>
-                    <Typography variant="h4" component="h4" style={{ fontWeight: "bold"}}>
-                        Collection Details Page
-                    </Typography>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Divider style={{ marginTop: 10, marginBottom: 10 }}/>
-                </Grid>
-                
-                <Grid item xs={12} sm={6} md={4} container justifyContent="center">
-                    <CollectionHeaderBodyDetail collection={collection} />
-                </Grid>
-
-                <Grid item xs={12} sm={6} md={8}>
-                    <CollectionBodyDetail collection={collection} onLike={likeHandler} user={user} />
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Divider style={{ marginTop: 10, marginBottom: 10 }}/>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Typography variant="h5" component="h5"
-                        style={{ fontWeight: "bold" }}
-                    >
-                        Stuff
-                    </Typography>
-
-                    <Grid container spacing={2}>
-                        { collection.stuff.length > 0 ?
-                            collection.stuff.map((item) => (
-                                <Grid item xs={12} md={6} lg={6} key={item._id} container justifyContent="center">
-                                    <StuffCard
-                                        id={item._id}
-                                        stuff={item}
-                                        isMine={isMine}
-                                    />
-                                </Grid>
-                            ))
-                        : 
-                            <Grid item xs={12} container justifyContent="center">
-                                <Typography variant="h6" component="h6" >
-                                    This collection is empty
-                                </Typography>
-                            </Grid>
-                        }
+        <>
+            <Grid container spacing={3} padding={2}>
+                <Grid item xs={0} md={1} lg={2}></Grid>
+                <Grid item xs={12} md={10} lg={8} container>
+                    <Grid item xs={12}>
+                        <Typography variant="h4" component="h4" style={{ fontWeight: "bold"}}>
+                            Collection Details Page
+                        </Typography>
                     </Grid>
-                </Grid>
 
+                    <Grid item xs={12}>
+                        <Divider style={{ marginTop: 10, marginBottom: 10 }}/>
+                    </Grid>
+                    
+                    <Grid item xs={12} sm={6} md={4} container justifyContent="center">
+                        <CollectionHeaderBodyDetail collection={collection} />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} md={8}>
+                        <CollectionBodyDetail collection={collection} onLike={likeHandler} user={user} onDelete={onDeleteHandler} />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Divider style={{ marginTop: 10, marginBottom: 10 }}/>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Typography variant="h5" component="h5"
+                            style={{ fontWeight: "bold" }}
+                        >
+                            Stuff
+                        </Typography>
+
+                        <StuffList stuff={collection.stuff} total={collection.totalStuff} isMe={isMine} user_id={user._id} setData={onClickPagination} url_base={
+                            `${process.env.REACT_APP_BACKEND_BASE_URL}/collections/${collection._id}?`
+                        }/>
+                    </Grid>
+
+                </Grid>
+                <Grid item xs={0} md={1} lg={2}></Grid>
             </Grid>
-            <Grid item xs={0} md={1} lg={2}></Grid>
-        </Grid>
+            {open && !error ? (
+                <Alert severity="success" open={open} handleClose={handleClose} message="Your collection has been deleted successfully!" />
+            )
+            : (
+                <Alert severity="error" open={open} handleClose={handleClose} message={error} />
+            )}
+        </>
     );
 };
 
