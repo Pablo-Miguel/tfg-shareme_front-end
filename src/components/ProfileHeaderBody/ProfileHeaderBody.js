8ff0234fe7837207c532a39b8e092349df43cdb0
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2023-06-06 20:15:33 +0200

    SM-22 Opens modal followers

--- src/components/ProfileHeaderBody/ProfileHeaderBody.js
+++ src/components/ProfileHeaderBody/ProfileHeaderBody.js
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
 import { Grid, Typography } from "@mui/material";
-
+import FollowersModal from "../FollowersModal/FollowersModal";
 
 const ProfileHeaderBody = ({frontUser}) => {
+
+    const [ open, setOpen ] = useState(false);
+    const [ type, setType ] = useState(null);
+    const [ title, setTitle ] = useState(null);
+
+
+    const onFollowersClickHandler = () => {
+        setTitle("Followers");
+        setType("followers");
+        setOpen(true);
+    };
+
+    const onFollowingClickHandler = () => {
+        setTitle("Following");
+        setType("following");
+        setOpen(true);
+    };
 
     return (
         <>
@@ -9,21 +27,22 @@ const ProfileHeaderBody = ({frontUser}) => {
                 <img src={frontUser.avatar} alt="stuff_image" style={{ minHeight: 300 }} />
             </Grid>
             <Grid item padding={2} container>
-                <Grid item xs={6} container justifyContent="center">
-                <Typography variant="p" component="p"
-                    style={{ display: "flex", alignItems: "center", gap: 5, fontWeight: "bold" }}
-                >
-                    {frontUser.followers} followers
-                </Typography>
+                <Grid item xs={6} container justifyContent="center" onClick={onFollowersClickHandler}>
+                    <Typography variant="p" component="p"
+                        style={{ display: "flex", alignItems: "center", gap: 5, fontWeight: "bold" }}
+                    >
+                        {frontUser.followers} followers
+                    </Typography>
                 </Grid>
-                <Grid item xs={6} container justifyContent="center">
-                <Typography variant="p" component="p"
-                    style={{ display: "flex", alignItems: "center", gap: 5, fontWeight: "bold" }}
-                >
-                    {frontUser.following} following
-                </Typography>
+                <Grid item xs={6} container justifyContent="center" onClick={onFollowingClickHandler}>
+                    <Typography variant="p" component="p"
+                        style={{ display: "flex", alignItems: "center", gap: 5, fontWeight: "bold" }}
+                    >
+                        {frontUser.following} following
+                    </Typography>
                 </Grid>
             </Grid>
+            <FollowersModal title={title} open={open} setOpen={setOpen} type={type} userId={frontUser._id} />
         </>
     );
 };
